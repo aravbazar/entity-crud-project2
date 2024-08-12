0fffced34c113ccdc5af6bdfce10fbfdad88e521
aravbazar <aravbazar01@gmail.com>
2024-08-12 14:44:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,12 @@ app.use('/api/entities', entityRoutes);
 app.use('/api/properties', propertyRoutes);
 app.use('/api/addresses', addressRoutes);
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Error connecting to MongoDB:', error));
 
